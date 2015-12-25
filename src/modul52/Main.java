e4e95888818597a364c15e6251a3d481d6784a9b
BliznyukS <bliznyuk.sergey@gmail.com>
2015-12-25 21:44:48 +0200

    DZ Modul 5 Sort Array

--- src/modul52/Main.java
+++ src/modul52/Main.java
@@ -1,4 +1,20 @@
 package modul52;
 
 public class Main {
+    public static void main(String[] args) {
+        int [] sortArray = {83, -3, 14, 67, 74, 56, 87};
+
+        for (int i = 0; i < sortArray.length - 1; i++){
+            if (sortArray[i] > sortArray[i + 1]){
+                int tmpVarArray = sortArray[i];
+
+                sortArray[i] = sortArray[i + 1];
+                sortArray[i + 1] = tmpVarArray;
+            }
+        }
+
+        for (int masiv : sortArray){
+            System.out.println(masiv);
+        }
+    }
 }
