02f6d8177aa33a019556471509b52ca6b7677e12
BliznyukS <bliznyuk.sergey@gmail.com>
2016-01-23 21:46:17 +0200

    DZ Modul 5 Bubble Sort

--- src/modul52/Main.java
+++ src/modul52/Main.java
@@ -5,19 +5,14 @@ public class Main {
         int[] resultSort;
         int[] resultSort1;
         int[] Array = {83, -3, 14, 67, 74, 56, 87};
-        int[] NewArray = {45, -10, 34, 1, 6, 5, 839, -1};
 
         ArraySort getSortedArray = new ArraySort();
         resultSort = getSortedArray.bubbleSort(Array);
-        resultSort1 = getSortedArray.bubbleSort(NewArray);
-
+        System.out.println(Array);
 
         for (int masiv : resultSort) {
             System.out.println(masiv);
         }
-        System.out.println("-----------------");
-        for (int masiv2 : resultSort1) {
-            System.out.println(masiv2);
-        }
+
     }
 }
