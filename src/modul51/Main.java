6386e3ab3646badaeb2122ca558f40976ce88960
BliznyukS <bliznyuk.sergey@gmail.com>
2016-01-23 22:13:27 +0200

    DZ Modul 5 Bubble Sort

--- src/modul51/Main.java
+++ src/modul51/Main.java
@@ -2,16 +2,22 @@ package modul51;
 
 public class Main {
     public static void main(String[] args) {
-        int [] array = {12,1,5,7,13, -1, -5};
+        int resultMin;
+        int resultMax;
+        int[] array = {12, 1, 5, 7, 13, -1, -5};
+        /*
         int max = 0;
         int min = 0;
-        for(int i = 0; i<array.length; i++){
-            if(max<array[i])
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i])
                 max = array[i];
-            if(min>array[i])
+            if (min > array[i])
                 min = array[i];
         }
-        System.out.println("Max: "+max);
-        System.out.println("Min:"+min);
+        */
+
+
+        System.out.println("Max: " + max);
+        System.out.println("Min:" + min);
     }
 }
--- src/modul51/MinMaxAlgorithm.java
+++ src/modul51/MinMaxAlgorithm.java
@@ -1,5 +1,22 @@
 package modul51;
 
 public class MinMaxAlgorithm {
-  //  public String
+    public int[] getMinElement(int[] array) {
+        int min = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i])
+                min = array[i];
+        }
+        return min;
+    }
+
+    public int[] getMaxElement(int[] array) {
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i])
+                max = array[i];
+        }
+        return max;
+    }
+
 }
